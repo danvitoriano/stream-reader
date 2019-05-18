77baeccc7abdceb8d98689add50a6a88f767fe26
Danilo Vitoriano <dvitoriano@uolinc.com>
2019-05-17 22:05:34 -0300

    add TestReadFile from a Stream Class

---
+++ src/main/java/br/com/fiap/TestReadFile.java
@@ -0,0 +1,23 @@
+package br.com.fiap;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class TestReadFile {
+    public static void main(String[] args) {
+        String filename = "/home/dvitoriano/workspace/fiap/stream-reader/lines.txt";
+
+
+        //read file into stream, try-with resources
+        try(Stream<String> stream = Files.lines(Paths.get(filename))){
+
+            stream.forEach(System.out::println);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
